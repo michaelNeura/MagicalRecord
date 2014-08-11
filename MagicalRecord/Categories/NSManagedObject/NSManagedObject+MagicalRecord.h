015b3b29701492e05d44fb7d0416165029cdbdb1
michaelNeura <Daniel@Daniels-MacBook-Pro.local>
2014-08-11 18:25:12 +0300

    fix header file

--- MagicalRecord/Categories/NSManagedObject/NSManagedObject+MagicalRecord.h
+++ MagicalRecord/Categories/NSManagedObject/NSManagedObject+MagicalRecord.h
@@ -30,7 +30,7 @@
 + (NSArray *) MR_propertiesNamed:(NSArray *)properties;
 
 + (id) MR_createEntity;
-+ (id) MR_createEntityInContext:(NSManagedObjectContext *);
++ (id) MR_createEntityInContext:(NSManagedObjectContext *)context;
 + (id) MR_createInContext:(NSManagedObjectContext *)context;
 - (BOOL) MR_deleteEntity;
 - (BOOL) MR_deleteInContext:(NSManagedObjectContext *)context;
